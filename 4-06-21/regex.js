b911bbf8aa34ad2cbac12c84c2f4fc339390dd7a
adiMallya <adimallya99@gmail.com>
2021-06-04 18:18:57 +0530

    added regex assessment

---
+++ 4-06-21/regex.js
@@ -0,0 +1,18 @@
+// regex for names
+// regex for department names
+// regex for USN
+
+const namesExp = /^[a - zA - Z\s]$/gi;
+const usnExp = /(1NT)(\d{2})((IS|CS|EC|ME|AE|EE))(\d{3})/gi
+const branchExp = /^(ISE|CSE|ECE|MEC|AE|EEE)$/gi;
+
+const testName = "Adithya Krishna S Mallya";
+console.log(testName.match(namesExp));
+
+const testUSN1 = "1nt18is013";
+const testUSN2 = "1NT18CS011";
+console.log(testUSN1.match(usnExp));
+console.log(testUSN2.match(usnExp));
+
+const testBranch = "ECE";
+console.log(testBranch.match(branchExp));
